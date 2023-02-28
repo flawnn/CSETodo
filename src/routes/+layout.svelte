e77384963289f93f06c8eb415a796e4accdad2d7
Flawn <fl4wn@protonmail.com>
2023-02-28 10:51:58 +0100

    🔒️ Add checks on load for DEK & Public key to ensure application null safety

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { onMount } from 'svelte';
 	import '../app.css';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
+
+	onMount(() => {
+		if (
+			localStorage.getItem('dek') == undefined ||
+			localStorage.getItem('public_key') == undefined
+		) {
+			localStorage.clear();
+
+			// Clearing cookies
+			document.cookie.split(';').forEach(function (c) {
+				document.cookie = c
+					.replace(/^ +/, '')
+					.replace(/=.*/, '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;');
+			});
+
+			location.reload();
+		}
+	});
 </script>
 
 <svelte:head>
