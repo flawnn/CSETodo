174e0cfd38e1ecc3c9592c6d7626f2a0521a2a0c
Flawn <fl4wn@protonmail.com>
2023-03-01 22:36:28 +0100

    🗃️ Add updateTodos() function

--- src/services/users.ts
+++ src/services/users.ts
@@ -8,118 +8,131 @@ import * as forge from 'node-forge';
 import type { JwtData } from './../types/JwtData';
 
 const createUser = async (session_id: string, dek: string, publicKey: forge.pki.rsa.PublicKey) => {
-    let public_key_pem = forge.pki.publicKeyToPem(publicKey)
+	let public_key_pem = forge.pki.publicKeyToPem(publicKey);
 
-    let existingUser = await db.users.findFirst({
-        where: {
-            public_key: public_key_pem
-        }
-    })
-    
-    // Generate default set of todos
-    var encrypted = encryptTodos(dek, [
+	let existingUser = await db.users.findFirst({
+		where: {
+			public_key: public_key_pem
+		}
+	});
+
+	// Generate default set of todos
+	var encrypted = encryptTodos(dek, [
 		{ id: '1', text: 'Todo 1', completed: true },
 		{ id: '2', text: 'Todo 2', completed: false },
 		{ id: '3', text: 'Todo 3', completed: false },
 		{ id: '4', text: 'Todo 4', completed: false }
 	]);
 
+	if (!existingUser) {
+		var newUser;
+		try {
+			type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 
-    if(!existingUser){
-        var newUser;
-        try {
-            type ThenArg<T> = T extends PromiseLike<infer U> ? U : T
+			newUser = (await db.users.create({
+				data: {
+					public_key: Base64.encode(public_key_pem),
+					active_sessions: [session_id],
+					dek: Base64.encode(publicKey.encrypt(dek)),
+					todos: encrypted
+				}
+			})) as Partial<ThenArg<ReturnType<typeof db.users.create>>>;
+		} catch {
+			return {
+				error: 'db_error'
+			};
+		}
 
-            newUser = await db.users.create({
-                data: {
-                    public_key: Base64.encode(public_key_pem),
-                    active_sessions: [session_id],
-                    dek: Base64.encode(publicKey.encrypt(dek)),
-                    todos: encrypted
-                }
-            }) as Partial<ThenArg<ReturnType<typeof db.users.create>>>
+		const jwtData: JwtData = {
+			id: newUser.id!,
+			session: session_id
+		};
 
-        } catch {
-            return {
-                error: "db_error"
-            }
-        }
+		const token = jwt.sign(jwtData, JWT_SECRET);
 
-        const jwtData: JwtData = {
-            id: newUser.id!,
-            session: session_id
-        }
-
-        const token = jwt.sign(jwtData, JWT_SECRET)
-
-        return {token}
-    } else {
-        return {
-            error: "public_key_used"
-        }
-    }
-}
+		return { token };
+	} else {
+		return {
+			error: 'public_key_used'
+		};
+	}
+};
 
 const findUser = async (id: string | undefined, public_key: string | undefined): Promise<users> => {
-    let key = id != null ? "id" : "public_key"
-    let query: {[k: string]: any} = {}
-    if(id != null){
-        query.id = id
-    } else if(public_key != null){
-        query.public_key = public_key
-    } else {
-        throw new Error("Missing params")
-    }
+	let key = id != null ? 'id' : 'public_key';
+	let query: { [k: string]: any } = {};
+	if (id != null) {
+		query.id = id;
+	} else if (public_key != null) {
+		query.public_key = public_key;
+	} else {
+		throw new Error('Missing params');
+	}
 
-    const user = await db.users.findUnique({
-        where: query
-    });
+	const user = await db.users.findUnique({
+		where: query
+	});
 
-    if (!user) {
-       throw new Error("User not found");
-    }
+	if (!user) {
+		throw new Error('User not found');
+	}
 
-    return user;
-}
+	return user;
+};
 
 const getUserByCookies = async (cookies: Record<string, string>): Promise<JwtData | null> => {
-    if (cookies["sessiontoken"]) {
-    const token = cookies["sessiontoken"];
+	if (cookies['sessiontoken']) {
+		const token = cookies['sessiontoken'];
 
-    try {
-      const jwtUser = jwt.verify(token, JWT_SECRET);
-      if (typeof jwtUser === "string") {
-        throw new Error("Something went wrong");
-      }
+		try {
+			const jwtUser = jwt.verify(token, JWT_SECRET);
+			if (typeof jwtUser === 'string') {
+				throw new Error('Something went wrong');
+			}
 
-    const user = await db.users.findUnique({
-    where: {
-        id: jwtUser.id,
-    },
-    });
+			const user = await db.users.findUnique({
+				where: {
+					id: jwtUser.id
+				}
+			});
 
-    if (!user) {
-    throw new Error("User not found");
-    }
+			if (!user) {
+				throw new Error('User not found');
+			}
 
-    const sessionUser: JwtData = {
-        id: user.id,
-        session: jwtUser.session as string,
-    } satisfies JwtData
+			const sessionUser: JwtData = {
+				id: user.id,
+				session: jwtUser.session as string
+			} satisfies JwtData;
 
-    return sessionUser;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+			return sessionUser;
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-  return null;
-}
+	return null;
+};
 
 const getTodos = async (id: string): Promise<string> => {
-    let dbUser = await findUser(id, undefined);
+	let dbUser = await findUser(id, undefined);
 
-    return dbUser.todos;
-}
-export { getTodos, createUser, getUserByCookies, findUser };
+	return dbUser.todos;
+};
 
+const updateTodos = async (encrypted_todos: string, id: string): Promise<void> => {
+	try {
+		const updateUser = await db.users.update({
+			where: {
+				id: id
+			},
+			data: {
+				todos: encrypted_todos
+			}
+		});
+	} catch (e) {
+		throw new Error('User not found');
+	}
+};
+
+export { getTodos, updateTodos, createUser, getUserByCookies, findUser };
