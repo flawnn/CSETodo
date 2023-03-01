a9062641019aece034e4b77062599327d2860b2f
Flawn <fl4wn@protonmail.com>
2023-03-01 23:20:33 +0100

    🗃️ Add POST Endpoint to /api/tasks

--- src/routes/api/tasks/+server.ts
+++ src/routes/api/tasks/+server.ts
@@ -1,9 +1,16 @@
-import { getTodos } from '$root/services/users';
-import type { RequestHandler } from "@sveltejs/kit";
+import { getTodos, updateTodos } from '$root/services/users';
+import { error, type RequestHandler } from "@sveltejs/kit";
 
 // Only two methods as we only update/get data over the server
-export const POST = (({ params, url }) => {
- 
+export const POST = (async ({ params, url, request, locals }) => {
+  let todos: string = await request.text();
+  
+      try {
+        updateTodos(todos, locals.user.id);
+			} catch (e) {
+        throw error(500)
+			}
+
   return new Response(String("GG"));
 }) satisfies RequestHandler;
 
