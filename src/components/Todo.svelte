6aac8e9f6a3c8d837525252d9ee92296f7af0a9c
Flawn <fl4wn@protonmail.com>
2023-03-01 23:19:34 +0100

    🗃️ Connect Todo BLoC with User model

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { encryptTodos, updateTodosFromServer } from '$root/lib/util';
 	import '$root/styles/todos.css';
 	import type { Todos } from '$root/types/Todo';
+	import { toast } from '@zerodevx/svelte-toast';
 	import { Base64 } from 'js-base64';
 	import * as forge from 'node-forge';
 	import { quintOut } from 'svelte/easing';
@@ -18,6 +20,62 @@
 	let public_key = forge.pki
 		.publicKeyFromPem(Base64.decode(localStorage.getItem('public_key') as string))
 		.n.toString(16);
+
+	// computed
+	$: todosAmount = todos.length;
+	$: pushTasksToDB(todos);
+
+	async function pushTasksToDB(newTodos: Todos[]) {
+		if (newTodos != initialTodos) {
+			initialTodos = await updateTodosFromServer(
+				newTodos,
+				data.user?.id!,
+				Base64.decode(localStorage.getItem('dek')!)
+			);
+
+			debugger;
+			let encrypted = encryptTodos(Base64.decode(localStorage.getItem('dek')!), newTodos);
+
+			let res = await fetch('/api/tasks', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: encrypted,
+				credentials: 'same-origin'
+			});
+
+			console.log(await res.text());
+			if (res.status == 500) {
+				toast.push('Error while updating Todos. Try adding another one or reload page.');
+			}
+		}
+
+		newTodos = todos;
+	}
+	// methods
+	function generateRandomId(): string {
+		return Math.random().toString(16).slice(2);
+	}
+
+	function addTodo(todo: string): void {
+		let newTodo: Todos = {
+			id: generateRandomId(),
+			text: todo,
+			completed: false
+		};
+		todos = [...todos, newTodo];
+	}
+
+	function toggleCompleted(event: MouseEvent): void {
+		let { checked } = event.target as HTMLInputElement;
+
+		todos = todos.map((todo) => ({
+			...todo,
+			completed: checked
+		}));
+	}
 </script>
 
 <main in:fade={{ duration: 1000 }}>
@@ -31,31 +89,33 @@
 	<div class="todos-container">
 		<h1 class="title">to-dos 🚧</h1>
 		<div class="todos">
-			<AddTodo />
-			<ul class="todo-list">
-				{#each todos as todo (todo.id)}
-					<li class="todo" transition:slide={{ delay: 250, duration: 300, easing: quintOut }}>
-						<div class="todo-item">
-							<div>
-								<input checked={todo.completed} id="todo" class="toggle" type="checkbox" />
-								<label aria-label="Check todo" class="todo-check" for="todo" />
+			<AddTodo {addTodo} {toggleCompleted} {todosAmount} />
+			{#if todosAmount}
+				<ul class="todo-list">
+					{#each todos as todo (todo.id)}
+						<li class="todo" transition:slide={{ delay: 250, duration: 300, easing: quintOut }}>
+							<div class="todo-item">
+								<div>
+									<input checked={todo.completed} id="todo" class="toggle" type="checkbox" />
+									<label aria-label="Check todo" class="todo-check" for="todo" />
+								</div>
+								<span class="todo-text">{todo.text}</span>
+								<button aria-label="Remove todo" class="remove" />
 							</div>
-							<span class="todo-text">{todo.text}</span>
-							<button aria-label="Remove todo" class="remove" />
-						</div>
-					</li>
-				{/each}
-			</ul>
+						</li>
+					{/each}
+				</ul>
 
-			<div class="actions">
-				<span class="todo-count">0 left</span>
-				<div class="filters">
-					<button class="filter">All</button>
-					<button class="filter">Active</button>
-					<button class="filter">Completed</button>
+				<div class="actions">
+					<span class="todo-count">0 left</span>
+					<div class="filters">
+						<button class="filter">All</button>
+						<button class="filter">Active</button>
+						<button class="filter">Completed</button>
+					</div>
+					<button class="clear-completed">Clear completed</button>
 				</div>
-				<button class="clear-completed">Clear completed</button>
-			</div>
+			{/if}
 		</div>
 	</div>
 </main>
