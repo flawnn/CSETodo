e780eabfa7fbff849a8c1c6ef25aba0e3c2dd9e5
Flawn <fl4wn@protonmail.com>
2023-04-14 20:54:09 +0200

    ✨ Add DBManager for testing purposes

---
+++ src/testing/db_manager.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const COLLECTIONS = ['users'];
+
+class DBManager {
+	db: any;
+	server: any;
+	connection: any;
+
+	constructor() {
+		this.db = null;
+		this.server = new MongoMemoryServer();
+		this.connection = null;
+	}
+
+	async start() {
+		const url = await this.server.getUri();
+		this.connection = new PrismaClient({
+			datasources: {
+				db: {
+					url: url
+				}
+			}
+		});
+
+		// this.db = this.connection.db(await this.server.getDbName());
+	}
+
+	stop() {
+		this.connection.close();
+		return this.server.stop();
+	}
+
+	cleanup() {
+		return Promise.all(COLLECTIONS.map((c) => this.db.collection(c).remove({})));
+	}
+}
+
+module.exports = DBManager;
