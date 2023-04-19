8b32e1ec9442bd667375436cc4867cf28efb6fff
Flawn <fl4wn@protonmail.com>
2023-04-19 18:18:58 +0200

    🧪 Fix E2E testing config

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -8,56 +8,56 @@ describe('Main Page (authenticated)', () => {
 	beforeEach(() => {
 		cy.setCookie('sessiontoken', Cypress.env('sessiontoken'), getDefaultCookieOptions());
 		cy.setCookie('client_id', Cypress.env('client_id'), getDefaultCookieOptions());
-					window.localStorage.setItem('public_key', Cypress.env('public_key'));
-			window.localStorage.setItem('dek', Cypress.env('dek'));
+		window.localStorage.setItem('public_key', Cypress.env('public_key'));
+		window.localStorage.setItem('dek', Cypress.env('dek'));
 	});
 
 	it('Loads Successfully', () => {
 		cy.visit('/');
 
-    cy.waitUntil(function() {
-      return Cypress.$('.spinner').length == 0
-  })
-    cy.contains("Sign-Up").should('not.exist')
+		cy.waitUntil(function () {
+			return Cypress.$('.spinner').length == 0;
+		});
+		cy.contains('Sign-Up').should('not.exist');
 	});
 });
 
 describe('Main Page (unauthenticated)', () => {
 	beforeEach(() => {
-		cy.clearCookies()
-		cy.clearAllLocalStorage()
-	})
+		cy.clearCookies();
+		cy.clearAllLocalStorage();
+	});
 
 	it('Onboarding', () => {
 		cy.visit('/');
-       cy.contains("Sign-Up").should('exist')
+		cy.contains('Sign-Up').should('exist');
 	});
 
 	it('Sign-up', () => {
 		cy.visit('/');
 
-		cy.intercept("/?/register").as("register-req")
-		cy.contains("Register").click()
+		cy.intercept('/?/register').as('register-req');
+		cy.contains('Register').click();
 
-		cy.wait("@register-req")
+		cy.wait('@register-req');
 		cy.location('pathname').should('include', '/');
-		
-		cy.contains("-----BEGIN RSA PRIVATE KEY-----")
-		cy.contains("SAVE this")
-	})
+
+		cy.contains('-----BEGIN RSA PRIVATE KEY-----');
+		cy.contains('SAVE this');
+	});
 
 	it('Log-in', () => {
 		cy.visit('/');
-		cy.contains('button', "Login").click()
+		cy.contains('button', 'Login').click();
 
-		cy.get('textarea').then(textArea => {
-      		textArea.text(Cypress.env("private_key"));
+		cy.get('textarea').then((textArea) => {
+			textArea.text(Cypress.env('private_key'));
 		});
 
-		cy.contains('button', "Login").click()
-		
-		cy.contains("Todo 1")
-		cy.get("#avatar").click()
-		cy.contains(/.*0xd8f9f*/)
-	})
+		cy.contains('button', 'Login').click();
+
+		cy.contains('Todo 1');
+		cy.get('#avatar').click();
+		cy.contains(/.*0xbebd3*/);
+	});
 });
