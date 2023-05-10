57a09ac80ecb905eca76329e77012e914e37ef8c
Flawn <fl4wn@protonmail.com>
2023-05-10 21:51:11 +0200

    🐛 Small bug regarding todo fixtures

--- src/testing/fixtures/components/fixtures.ts
+++ src/testing/fixtures/components/fixtures.ts
@@ -1,7 +1,8 @@
+import type { Todos } from '$root/types/helper/Todo';
 import { Config } from '../../../config';
 
 export const ComponentFixtures = {
 	todos: Config.defaultTodos,
 	todosAmount: 4,
-	todo: [{ id: '420', text: 'TEST', completed: true }]
+	todo: { id: '420', text: 'TEST', completed: true } satisfies Todos
 };
